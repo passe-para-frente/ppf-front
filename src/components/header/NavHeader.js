0dc8c6f9048b5c56db2e903c094d2b4bf707d48f
Roger <12768403+rogix@users.noreply.github.com>
2019-06-13 18:57:23 -0300

    excluindo component do header

--- src/components/header/NavHeader.js
+++ src/components/header/NavHeader.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import ContentHeader from './ContentHeader';
 
 export default class NavHeader extends Component {
   render() {
     return (
-      <>
-        <div className="navbar-fixed">
-          <nav className="green">
-            <div className="nav-wrapper container">
-              <a href="#home" className="brand-logo">Passe Para Frente</a>
-                <ul id="nav-mobile" className="right hide-on-med-and-down">
-                  <li><a href="#sobre">Sobre</a></li>
-                  <li><a href="#receber">Receber</a></li>
-                  <li><a href="#contato">Contato</a></li>
-                  <li><a href="#login">Login</a></li>
-                </ul>
-            </div>
-          </nav>
-        </div>
-        <ContentHeader />
-      </>
+      <div className="navbar-fixed">
+        <nav className="green">
+          <div className="nav-wrapper container">
+            <a href="#home" className="brand-logo">Passe Para Frente</a>
+              <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li><a href="#sobre">Sobre</a></li>
+                <li><a href="#receber">Receber</a></li>
+                <li><a href="#contato">Contato</a></li>
+                <li><a href="#login">Login</a></li>
+              </ul>
+          </div>
+        </nav>
+      </div>
     );
   }
 }
