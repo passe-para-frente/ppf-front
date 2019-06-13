18bceb4e40ed70d14a0839b85d7756fc4e6518fd
Roger <12768403+rogix@users.noreply.github.com>
2019-06-13 12:39:58 -0300

    home component

---
+++ src/components/home/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+
+import Header from './header/NavHeader';
+
+// import { Container } from './styles';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <Header />
+    );
+  }
+}
