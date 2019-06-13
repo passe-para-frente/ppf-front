a2c5b9ad316e078399cfad066c5cacf0a819f16a
Roger <12768403+rogix@users.noreply.github.com>
2019-06-13 14:48:45 -0300

    rotas configuradas

---
+++ src/app/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import Routes from '../routes';
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+  );
+}
+
+export default App;
