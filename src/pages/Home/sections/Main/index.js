0fbfd1b2b94aa97a8a4fbf86c0a4feb5cf443610
Roger <12768403+rogix@users.noreply.github.com>
2019-08-17 17:06:24 -0300

    chores: add some fix styles to main section

--- src/pages/Home/sections/Main/index.js
+++ src/pages/Home/sections/Main/index.js
@@ -17,15 +17,15 @@ export default function Main() {
           src={LogoMain}
           alt="Logo Passe Para a Frente"
         />
-        <p>
-          <strong>Passe para Frente</strong> é um site de{' '}
-          <strong>doação de material esportivo</strong>, livro ou vestuário onde
-          pessoas físicas e jurídicas podem <strong>doar</strong> a fim de
-          contribuir com instituições, escolas da rede pública de ensino e
-          crianças. Queremos com isso estimular a cultura de doação no país.
-        </p>
-        <div className="saiba-mais">
-          <strong>SAIBA MAIS</strong>
+        <div>
+          <p>
+            <strong>Passe para Frente</strong> é um site de{' '}
+            <strong>doação de material esportivo</strong>, livro ou vestuário
+            onde pessoas físicas e jurídicas podem <strong>doar</strong> a fim
+            de contribuir com instituições, escolas da rede pública de ensino e
+            crianças. Queremos com isso estimular a cultura de doação no país.
+          </p>
+          <button type="button">SAIBA MAIS</button>
         </div>
       </Content>
     </Container>
--- src/pages/Home/sections/Main/styles.js
+++ src/pages/Home/sections/Main/styles.js
@@ -2,16 +2,50 @@ import styled from 'styled-components';
 
 export const Container = styled.section`
   position: relative;
-  height: 100vh;
 
   > img {
     width: 100%;
     margin-top: 70px;
   }
+
+  .cover {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 4px;
+    left: 0;
+    background: rgba(2, 2, 2, 0.2);
+  }
 `;
 
 export const Content = styled.div`
   position: absolute;
-  top: 20%;
-  width: 100%;
+  width: 80%;
+  top: 80px;
+  left: 0;
+  right: 0;
+  margin-right: auto;
+  margin-left: auto;
+
+  > img {
+    width: 50%;
+  }
+
+  div {
+    position: absolute;
+    width: 500px;
+    right: 0;
+    color: WHEAT;
+    line-height: 1.5;
+    font-size: 16px;
+
+    button {
+      border: 3px solid #8bc541;
+      padding: 10px 25px;
+      background: 0;
+      color: #f2f2f2;
+      font-weight: 900;
+      margin-top: 10px;
+    }
+  }
 `;
