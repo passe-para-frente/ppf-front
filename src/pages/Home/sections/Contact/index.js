d161ea9c91357d11e24882eb7b4f28de9f60d12a
Roger <12768403+rogix@users.noreply.github.com>
2019-08-19 23:38:02 -0300

    chores: start contact section

--- src/pages/Home/sections/Contact/index.js
+++ src/pages/Home/sections/Contact/index.js
@@ -1,16 +1,38 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
 
-import { Container } from './styles';
+import { Container, ContactHeader, ContactInfo, ContactForm } from './styles';
 
-import logo from '../../../../assets/images/logo.png';
+import logo from '../../../../assets/images/contact/logo-black.png';
 
 export default function Contact() {
   return (
-    <Container>
-      <h1>Entre em contato</h1>
-      <div>
-        <img src={logo} alt="" />
-      </div>
-    </Container>
+    <>
+      <ContactHeader>Entre em contato</ContactHeader>
+      <Container>
+        <ContactInfo>
+          <div>
+            <img src={logo} alt="" />
+          </div>
+          <p>
+            Aquele material esportivo, livro ou vestuário e ajude
+            <br />
+            milhares de escolas e crianças. E o melhor: não precisa
+            <br />
+            sair de casa para doar. Nós retiramos o material em seu <br />
+            endereço e o entregamos à instituição escolhida por você.
+          </p>
+        </ContactInfo>
+        <ContactForm>
+          <Form>
+            <Input name="name" />
+            <Input name="email" />
+            <Input multiline name="bio" />
+
+            <button type="submit">Send</button>
+          </Form>
+        </ContactForm>
+      </Container>
+    </>
   );
 }
--- src/pages/Home/sections/Contact/styles.js
+++ src/pages/Home/sections/Contact/styles.js
@@ -1,5 +1,22 @@
 import styled from 'styled-components';
+import { Form, Input } from '@rocketseat/unform';
 
-export const Container = styled.section``;
+export const Container = styled.section`
+  width: 980px;
+  border: red solid 1px;
+  margin: 0 auto;
+  display: flex;
+`;
 
-export const Header = styled.header``;
+export const ContactHeader = styled.h1`
+  color: red;
+  display: block;
+`;
+
+export const ContactInfo = styled.div``;
+
+export const ContactForm = styled.div``;
+
+export const Header = styled(Form)`
+  color: red;
+`;
